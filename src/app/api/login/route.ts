b33b191dfd7fec4724e8e3a7cfe8c9ebbae046f8
Dumboiroy <alroylohzenyang@gmail.com>
2025-06-12 17:39:14 +0800

    small stuff only

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -7,12 +7,8 @@ export async function POST(req: NextRequest) {
     // just for the dummy
     // would replace with if the username in database
     if (username === 'admin' && password === 'secret') {
-        return NextResponse.json(
-            { status: 200 },
-        );
+        return NextResponse.json({ status: 200 });
     }
 
-    return NextResponse.json(
-        { status: 401 },
-    );
+    return NextResponse.json({ status: 401 });
 }
