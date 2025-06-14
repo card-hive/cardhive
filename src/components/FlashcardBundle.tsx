1d043b5497f0516cf14d98b52505e872695b897a
Dumboiroy <alroylohzenyang@gmail.com>
2025-06-14 15:05:55 +0800

    cardset page hydration fixed

--- src/components/FlashcardBundle.tsx
+++ src/components/FlashcardBundle.tsx
@@ -17,8 +17,18 @@ export default function FlashcardBundle({
             href={`/cardview/${cardset_id}`}
             className="bg-white rounded-2xl shadow-md overflow-hidden w-full flex flex-col items-center"
         >
-            <Image src={image} alt={title} width={200} height={150} />
-            <h3>{title}</h3>
+            <div className="relative w-full h-32">
+                <Image
+                    src={image}
+                    alt={title}
+                    fill
+                    style={{ objectFit: 'cover' }}
+                    sizes="100vw"
+                />
+            </div>
+            <div className="bg-violet-300 text-black text-center font-semibold py-2 w-full">
+                {title}
+            </div>
         </Link>
     );
 }
