eac8c0ba2c646e771c3302c6ebcbc0cefe0333e1
Dumboiroy <alroylohzenyang@gmail.com>
2025-06-14 14:49:22 +0800

    Change Landing Page to Modules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-    return (
-        <div>
-            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-                <h1>Homepage</h1>
-            </main>
-            <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
-        </div>
-    );
+    redirect('/modules');
+    // return (
+    //     <div>
+    //         <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    //             <h1>Homepage</h1>
+    //         </main>
+    //         <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
+    //     </div>
+    // );
 }
